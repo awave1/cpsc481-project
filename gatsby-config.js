c9cd3ed47534c641df956d8796becd76c58e57f2
awave1 <artgolovin96@gmail.com>
2019-01-25 11:35:30 -0700

    Remove gatsby boilerplate stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/cpsc481-project',
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CPSC481 Project`,
+    description: ``,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
